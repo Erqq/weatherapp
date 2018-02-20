7d81196835ec0c33caed42e12264db7b942fd998
Eerik <eerik.timonen94@gmail.com>
2018-02-20 20:05:29 +0200

    Add eslint feature
    Add eslint feature so user can run eslint and check errors

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,16 +1,11 @@
-const debug = require('debug')('weathermap');
-
 const Koa = require('koa');
 const router = require('koa-router')();
 const fetch = require('node-fetch');
 const cors = require('kcors');
-
 const appId = process.env.APPID || '';
-const mapURI = process.env.MAP_ENDPOINT || "http://api.openweathermap.org/data/2.5";
-const targetCity = process.env.TARGET_CITY || "Helsinki,fi";
-
+const mapURI = process.env.MAP_ENDPOINT || 'http://api.openweathermap.org/data/2.5';
+const targetCity = process.env.TARGET_CITY || 'Tampere,fi';
 const port = process.env.PORT || 9000;
-
 const app = new Koa();
 
 app.use(cors());
@@ -18,20 +13,21 @@ app.use(cors());
 const fetchWeather = async () => {
   const endpoint = `${mapURI}/forecast?q=${targetCity}&appid=${appId}&`;
   const response = await fetch(endpoint);
-
-  return response ? response.json() : {}
+  return response ? response.json() : {};
 };
 
 router.get('/api/weather', async ctx => {
   const weatherData = await fetchWeather();
-
   ctx.type = 'application/json; charset=utf-8';
-  ctx.body = weatherData ? weatherData : {};
+  ctx.body = weatherData || {};
+});
+router.post('/api/weatherloc', async ctx => {
+  const weatherData = await fetchWeather();
+  ctx.type = 'application/json; charset=utf-8';
+  ctx.body = weatherData || {};
 });
 
 app.use(router.routes());
 app.use(router.allowedMethods());
-
 app.listen(port);
-
 console.log(`App listening on port ${port}`);
--- frontend/src/index.jsx
+++ frontend/src/index.jsx
@@ -13,32 +13,41 @@ const getWeatherFromApi = async () => {
 
   return {};
 };
-
+const getLocation = async () => {
+  if ("geolocation" in navigator) {
+    navigator.geolocation.getCurrentPosition(function(position) {
+      const location = {lat:position.coords.latitude, long: position.coords.longitude};
+      return location
+     
+    });
+  } else {
+    /* geolocation IS NOT available */
+  }
+  return {};
+}
 class Weather extends React.Component {
   constructor(props) {
     super(props);
-
+    getLocation();
     this.state = {
-      icon: "",
+      icon: '',
     };
   }
 
   async componentWillMount() {
     const weather = await getWeatherFromApi();
-    console.log(weather);
-    this.setState({city: weather.city.name})
-    this.setState({icon: weather.list.slice(0,3).map(item =>{return ([<h2>{item.dt_txt}</h2>,<img src={`/img/${item.weather[0].icon.slice(0, -1)}.svg`}/>])})});
-    
+    this.setState({ city: weather.city.name });
+    this.setState({ icon: weather.list.slice(0, 3).map(item => ([<h2>{item.dt_txt}</h2>, <img src={`/img/${item.weather[0].icon.slice(0, -1)}.svg`} alt="" />])) });
   }
-  
+
   render() {
     const { icon } = this.state;
-    const { city} = this.state;
+    const { city } = this.state;
     return (
       <div className="icon">
-       <h1>{city}</h1>
-      { icon }
-     
+        <h1>{city}</h1>
+        { icon }
+
       </div>
     );
   }
