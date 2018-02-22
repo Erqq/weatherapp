173097fbaef19c83c79402004ea0d882df7095f1
Eerik <eerik.timonen94@gmail.com>
2018-02-22 18:36:37 +0200

    Test google cloud

--- frontend/webpack.production.config.js
+++ frontend/webpack.production.config.js
@@ -30,7 +30,7 @@ module.exports = {
     ], '.'),
     new webpack.DefinePlugin({
       'process.env': {
-        ENDPOINT: JSON.stringify(process.env.ENDPOINT || 'http://0.0.0.0:9000/api'),
+        ENDPOINT: JSON.stringify(process.env.ENDPOINT || '127.0.0.1:9000/api'),
       },
     }),
   ],
